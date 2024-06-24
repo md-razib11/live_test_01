ae4da5f0dc7f1217ec8af3cc402be728da289f04
Md Razib <74146449+md-razib11@users.noreply.github.com>
2024-06-24 23:16:32 +0600

    first commit

---
+++ bin/class.dart
@@ -0,0 +1,15 @@
+class Media {
+  void play() {
+    print('Playing media...');
+  }
+}
+
+class Song extends Media {
+  String artist;
+  Song(this.artist);
+
+  @override
+  void play() {
+    print('Playing song by $artist...');
+  }
+}
